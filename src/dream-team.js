0724cd5d1867317f04205cf3818a30478677efe0
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-05-01 03:16:11 +0700

    feat: complite Count cats and Carbon dating

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (members.length === 0) {
+    return false;
+  }
+
+  members = members.sort();
+
+  const resultArray = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (members[i] != "string" || members[i].length === null) {
+      continue;
+    }
+  }
+
+  const res2 = [];
+
+  for (let i = 0; i < members.length; i++) {
+    res2.push(members[i][0].toUpperCase());
+  }
+
+  return res2.toString();
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
